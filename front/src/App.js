8699cc7630bb2b6d48e51fbe2dfc057ce621d56f
Ely Lam <eydelamb14@gmail.com>
2020-12-13 22:23:47 -0800

    Description edit

--- front/src/App.js
+++ front/src/App.js
@@ -56,7 +56,7 @@ function App() {
 
         <br/>
         <footer>
-          <p>Favicon made by <a href="https://www.flaticon.com/authors/becris" title="Becris">Becris</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+          <p>Icons made by <a href="https://www.flaticon.com/authors/becris" title="Becris">Becris</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
         </footer>
       </div>
     </Router>
